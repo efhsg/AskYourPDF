fcae6732c0de3e6c922f26eab4e3df6e3f38acbe
esg <efhsg@live.nl>
2024-11-22 18:09:41 +0100

    cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# AskYourPDF
-Simple Python script to ask questions to a PDF, using Docker-compose and OpenAI
--- src/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run the command passed as argument or start a shell
-if [ $# -gt 0 ]; then
-    exec "$@"
-else
-    echo 'Running...' && tail -f /dev/null
-fi
